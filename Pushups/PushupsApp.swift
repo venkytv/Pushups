8ccaffa1f539e8f801cf5ba8e71f9a79c5ed1ac9
Venky Tumkur <venkytv@gmail.com>
2022-06-19 07:57:06 +0200

    Initial Commit

---
+++ Pushups/PushupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushupsApp.swift
+//  Pushups
+//
+//  Created by Venky Tumkur on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct PushupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
